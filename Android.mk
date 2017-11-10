8d28e6ff949764aad4eb38bc7a9b35d3875ea428
sdv22 <sdv22@live.ru>
2017-11-10 09:53:00 +0300

    Removed MatLog

--- Android.mk
+++ Android.mk
@@ -271,19 +271,6 @@ LOCAL_MODULE_SUFFIX := .apk
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE := MatLog
-LOCAL_MODULE_OWNER := leeco
-LOCAL_SRC_FILES := proprietary/priv-app/MatLog/MatLog.apk
-LOCAL_CERTIFICATE := platform
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := APPS
-LOCAL_DEX_PREOPT := false
-LOCAL_MODULE_SUFFIX := .apk
-LOCAL_PRIVILEGED_MODULE := true
-include $(BUILD_PREBUILT)
-
-
-include $(CLEAR_VARS)
 LOCAL_MODULE := Ax
 LOCAL_MODULE_OWNER := leeco
 LOCAL_SRC_FILES := proprietary/priv-app/Ax/Ax.apk
--- x2-vendor.mk
+++ x2-vendor.mk
@@ -1030,5 +1030,4 @@ PRODUCT_PACKAGES += \
     qti-vzw-ims-internal \
     qdcm_calib_data_le_x2_mdss_dsi_sharp_qhd_dualdsi_cmd \
     qdcm_calib_data_le_x2_mdss_dsi_truly_qhd_dualdsi_cmd_pvt \
-    MatLog \
     qdcm_calib_data_mdss_dsi_sharp_qhd_dualdsi_cmd
